737ebf01227101bc4b96d7ff2ad5314e2eb9bed9
Marc Gronle <marc.gronle@gmx.de>
2013-03-23 14:50:25 +0100

    duplicated license comments at beginning of files deleted

--- Qitom/widgets/scriptEditorWidget.cpp
+++ Qitom/widgets/scriptEditorWidget.cpp
@@ -18,26 +18,6 @@
 
     You should have received a copy of the GNU Library General Public License
     along with itom. If not, see <http://www.gnu.org/licenses/>.
-*********************************************************************** *//* ********************************************************************
-   itom measurement system
-   URL: http://www.uni-stuttgart.de/ito
-   Copyright (C) 2012, Institut für Technische Optik (ITO),
-   Universität Stuttgart, Germany
-
-   This file is part of itom.
-
-   itom is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   itom is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with itom. If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************** */
 
 #include "../python/pythonEngineInc.h"
