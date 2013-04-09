3f9270cc4c292f515359e9c2500dcab7268c8ea1
Marc Gronle <gronle@ito.uni-stuttgart.de>
2013-04-09 14:14:53 +0200

    bugfix due to previous merge error

--- Qitom/python/pythonItom.cpp
+++ Qitom/python/pythonItom.cpp
@@ -28,6 +28,7 @@
 #include "pythonCommon.h"
 #include "pythonProxy.h"
 #include "pythonFigure.h"
+#include "pythonPlotItem.h"
 
 #include "pythonEngine.h"
 
@@ -212,7 +213,9 @@ PyObject* PythonItom::PyPlotImage(PyObject * /*pSelf*/, PyObject *pArgs, PyObjec
     QString defaultPlotClassName;
     if(className) defaultPlotClassName = className;
 
-    QMetaObject::invokeMethod(uiOrg, "figurePlot", Q_ARG(QSharedPointer<ito::DataObject>, newDataObj), Q_ARG(QSharedPointer<unsigned int>, figHandle), Q_ARG(int, areaRow), Q_ARG(int, areaCol), Q_ARG(QString, defaultPlotClassName), Q_ARG(ItomSharedSemaphore*,locker.getSemaphore()));
+    QSharedPointer<unsigned int> objectID(new unsigned int);
+
+    QMetaObject::invokeMethod(uiOrg, "figurePlot", Q_ARG(QSharedPointer<ito::DataObject>, newDataObj), Q_ARG(QSharedPointer<unsigned int>, figHandle), Q_ARG(QSharedPointer<unsigned int>, objectID), Q_ARG(int, areaRow), Q_ARG(int, areaCol), Q_ARG(QString, defaultPlotClassName), Q_ARG(ItomSharedSemaphore*,locker.getSemaphore()));
     if (!locker.getSemaphore()->wait(PLUGINWAIT))
     {
         return PyErr_Format(PyExc_RuntimeError, "timeout while plotting data object");
@@ -223,7 +226,23 @@ PyObject* PythonItom::PyPlotImage(PyObject * /*pSelf*/, PyObject *pArgs, PyObjec
         return NULL;
     }
     
-    return Py_BuildValue("i", *figHandle); //returns handle
+    //return Py_BuildValue("iO", *figHandle); //returns handle
+
+    //return new instance of PyUiItem
+    PyObject *args2 = PyTuple_New(0); //Py_BuildValue("OO", self, name);
+    PyObject *kwds2 = PyDict_New();
+    PyDict_SetItemString(kwds2, "objectID", PyLong_FromLong(*objectID));
+    PythonPlotItem::PyPlotItem *pyPlotItem = (PythonPlotItem::PyPlotItem *)PyObject_Call((PyObject *)&PythonPlotItem::PyPlotItemType, args2, kwds2);
+    Py_DECREF(args2);
+    Py_DECREF(kwds2);
+
+    if(pyPlotItem == NULL)
+    {
+        PyErr_SetString(PyExc_AttributeError, "Could not create plotItem of plot widget");
+        return NULL;
+    }
+
+    return Py_BuildValue("iO", *figHandle, (PyObject*)pyPlotItem); //returns handle
 }
 
 ////----------------------------------------------------------------------------------------------------------------------------------
@@ -326,7 +345,9 @@ PyObject* PythonItom::PyLiveImage(PyObject * /*pSelf*/, PyObject *pArgs, PyObjec
     QString defaultPlotClassName;
     if(className) defaultPlotClassName = className;
 
-    QMetaObject::invokeMethod(uiOrg, "figureLiveImage", Q_ARG(AddInDataIO*, cam->dataIOObj), Q_ARG(QSharedPointer<unsigned int>, figHandle), Q_ARG(int, areaRow), Q_ARG(int, areaCol), Q_ARG(QString, defaultPlotClassName), Q_ARG(ItomSharedSemaphore*,locker.getSemaphore()));
+    QSharedPointer<unsigned int> objectID(new unsigned int);
+
+    QMetaObject::invokeMethod(uiOrg, "figureLiveImage", Q_ARG(AddInDataIO*, cam->dataIOObj), Q_ARG(QSharedPointer<unsigned int>, figHandle), Q_ARG(QSharedPointer<unsigned int>, objectID), Q_ARG(int, areaRow), Q_ARG(int, areaCol), Q_ARG(QString, defaultPlotClassName), Q_ARG(ItomSharedSemaphore*,locker.getSemaphore()));
     if (!locker.getSemaphore()->wait(PLUGINWAIT))
     {
         return PyErr_Format(PyExc_RuntimeError, "timeout while showing live image of camera");
@@ -337,7 +358,23 @@ PyObject* PythonItom::PyLiveImage(PyObject * /*pSelf*/, PyObject *pArgs, PyObjec
         return NULL;
     }
     
-    return Py_BuildValue("i", *figHandle); //returns handle
+    //return Py_BuildValue("i", *figHandle); //returns handle
+
+    //return new instance of PyUiItem
+    PyObject *args2 = PyTuple_New(0); //Py_BuildValue("OO", self, name);
+    PyObject *kwds2 = PyDict_New();
+    PyDict_SetItemString(kwds2, "objectID", PyLong_FromLong(*objectID));
+    PythonPlotItem::PyPlotItem *pyPlotItem = (PythonPlotItem::PyPlotItem *)PyObject_Call((PyObject *)&PythonPlotItem::PyPlotItemType, args2, kwds2);
+    Py_DECREF(args2);
+    Py_DECREF(kwds2);
+
+    if(pyPlotItem == NULL)
+    {
+        PyErr_SetString(PyExc_AttributeError, "Could not create plotItem of plot widget");
+        return NULL;
+    }
+
+    return Py_BuildValue("iO", *figHandle, (PyObject*)pyPlotItem); //returns handle
 }
 
 ////----------------------------------------------------------------------------------------------------------------------------------
