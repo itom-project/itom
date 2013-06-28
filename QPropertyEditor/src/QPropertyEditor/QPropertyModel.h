bda0517fac13195a7e5dc0cadc621bd44c339b92
Marc Gronle <gronle@ito.uni-stuttgart.de>
2013-06-28 17:13:56 +0200

    license of QPropertyEditor changed to LGPL (see http://qt-apps.org/content/show.php/QPropertyEditor?content=68684)

--- QPropertyEditor/src/QPropertyEditor/QPropertyModel.h
+++ QPropertyEditor/src/QPropertyEditor/QPropertyModel.h
@@ -1,23 +1,25 @@
 // *************************************************************************************************
 //
 // QPropertyEditor v 0.3
-//   
+//
 // --------------------------------------
 // Copyright (C) 2007 Volker Wiendl
 // Acknowledgements to Roman alias banal from qt-apps.org for the Enum enhancement
 //
 //
 // The QPropertyEditor Library is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation version 3 of the License 
+// it under the terms of the GNU Lesser General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
 //
-// The Horde3D Scene Editor is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+// Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+// http://www.gnu.org/copyleft/lesser.txt.
 //
 // *************************************************************************************************
 #ifndef QPROPERTYMODEL_H_
