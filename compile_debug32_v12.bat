cb5fd03839e4999017f13ef9d31a2bd481de45b2
Kohler <kohler@twip-os,com>
2015-02-04 18:12:20 +0100

    build bat files for other MSVC versions

---
+++ compile_debug32_v12.bat
@@ -0,0 +1,7 @@
+CALL "%VS120COMNTOOLS%\vsvars32.bat" x86
+
+msbuild.exe %~dp0\ALL_BUILD.vcxproj /p:configuration=debug /p:platform=win32
+
+pause
+
+REM for rebuild add "/t:rebuild" after /p:platform=win32
