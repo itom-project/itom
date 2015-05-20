3e7bce8cca26171d8112195982fe19db2d06b917
Marc Gronle <gronle@ito.uni-stuttgart.de>
2015-05-20 10:10:12 +0200

    modifications of tutorial samples

--- demo/tutorial/cross-correlation/start_dialog.py
+++ demo/tutorial/cross-correlation/start_dialog.py
@@ -21,11 +21,27 @@ def evaluate():
     [dx,dy] = calc_correlation.evaluate(image1,image2)
     gui.lbl_dx["text"] = "dx: " + str(dx)
     gui.lbl_dy["text"] = "dy: " + str(dy)
+    
+def saveImages():
+    filename = ui.getSaveFileName("Filename", filters="IDC (*.idc)", parent = gui)
+    if filename:
+        saveIDC(filename, {"image1":image1, "image2":image2})
+        
+def loadImages():
+    global image1
+    global image2
+    filename = ui.getOpenFileName("Filename", filters = "IDC (*idc)", parent = gui)
+    if filename:
+        d = loadIDC(filename)
+        image1 = d["image1"]
+        image2 = d["image2"]
+        gui.plot1["source"] = image1
+        gui.plot2["source"] = image2
 
 
 
 #open camera (make it before you start this script)
-cam = dataIO("OpenCVGrabber", colorMode = "gray")
+cam = dataIO("MSMediaFoundation", colorMode = "gray")
 #cam = dataIO("FileGrabber","*.tif","samples",8,2)
 
 #start camera
@@ -39,6 +55,8 @@ image2 = dataObject()
 gui = ui("dialog.ui", ui.TYPEWINDOW)
 gui.btnAcquire1.connect("clicked()", acquireImage1)
 gui.btnAcquire2.connect("clicked()", acquireImage2)
+gui.btnLoad.connect("clicked()", loadImages)
+gui.btnSave.connect("clicked()", saveImages)
 gui.btnEval.connect("clicked()", evaluate)
 
 #show live image in upper plot
