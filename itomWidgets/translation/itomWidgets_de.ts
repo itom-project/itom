9f6d759454a17728dbf2bb8fe3705c7e20c65b0a
itobiege <itobiege@ito.uni-stuttgart.de>
2015-07-13 14:27:58 +0200

    updated German translation

---
+++ itomWidgets/translation/itomWidgets_de.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>DoubleRangeWidget</name>
+    <message>
+        <source>SliderSpinBoxWidget</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MenuComboBox</name>
+    <message>
+        <source>Search...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MenuComboBoxPrivate</name>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PathLineEdit</name>
+    <message>
+        <source>Select a file to save </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PathLineEditPrivate</name>
+    <message>
+        <source>Open a dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RangeWidget</name>
+    <message>
+        <source>SliderSpinBoxWidget</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchBoxPrivate</name>
+    <message>
+        <source>Search...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SliderWidget</name>
+    <message>
+        <source>SliderWidget</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
