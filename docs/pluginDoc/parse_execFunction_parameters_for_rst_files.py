ede76951d4779453b51056c3fd363d6670424adb
Johann Krauter <johannkrauter@gmail.com>
2016-01-14 18:35:45 +0100

    execfunction documantation will be created to be intepreted as numpy function style.

--- docs/pluginDoc/parse_execFunction_parameters_for_rst_files.py
+++ docs/pluginDoc/parse_execFunction_parameters_for_rst_files.py
@@ -10,24 +10,57 @@ def parse_parameters(instance):
     
     for funcdictName, funcdictValue in funcdict.items(): #get mandatory/ optional/ output parameters
         paramtypedict = instance.getExecFuncsInfo(funcdictName, detailLevel = 1)
-        item = ".. py:function:: instance.exec('%s'  [, mandatoryParameters, optionalParameter , outputParameter])\n" %(funcdictName)
-        result.append(item)
         
-        item = "    %s\n" %(funcdictValue)
-        result.append(item)
+        mandParamsString = ""
+        optParamsString = ""
+        outParamsString = ""
         
-        
-        # separate parameter for parameter type
-        for typeName, typeValue in paramtypedict.items():
-            item = "%s:\n" %(typeName)
-            result.append(item)
+        if "Mandatory Parameters" in paramtypedict.keys():
+            mandParamsString = ", ".join([d["name"] for d in paramtypedict["Mandatory Parameters"]]) 
+        if "Optional Parameters" in paramtypedict.keys():
+            optParamsString = " [," + ", ".join([d["name"] for d in paramtypedict["Optional Parameters"]]) +"]"
+        #if "Output Parameters" in paramtypedict.keys():
+            #outParamsString = " ".join([d["name"] for d in paramtypedict["Output Parameters"]]) +" = "
             
-            for paramtypename in typeValue:
-                item = "**%s**: {%s}\n    %s\n" % (paramtypename["name"], paramtypename["type"], paramtypename["info"])
-                result.append(item)
+        item = "\n.. py:function:: " + outParamsString + " instance.exec('%s', "%(funcdictName) + mandParamsString + optParamsString + ")\n\n"
+        result.append(item)
         
-    print("\n".join(result))
+        item = "    %s\n\n" %(funcdictValue)
+        result.append(item)
+        mandParamsString = ""
+        optParamsString = ""
+        outParamsString = ""
         
-
+        mandParams = []
+        optParams = []
+        outParams = []
+        
+        if "Mandatory Parameters" in paramtypedict.keys():
+            mandParams = paramtypedict["Mandatory Parameters"]
+        if "Optional Parameters" in paramtypedict.keys():
+            optParams = paramtypedict["Optional Parameters"]
+        if "Output Parameters" in paramtypedict.keys():
+            outParams = paramtypedict["Output Parameters"]
+        
+        mandSignature = ", ".join([i["name"] for i in mandParams])
+        optSignature = ", ".join([i["name"] for i in optParams])
+        signature = mandSignature
+        if signature != "" and optSignature != "":
+            signature += "[, " + optSignature + "]"
+        elif optSignature != "":
+            signature = "[" + optSignature + "]"
+        
+        if signature != "":
+            for i in mandParams:
+                result.append("    :param %s: %s\n    :type %s: %s\n" % (i["name"], i["info"], i["name"], i["type"]))
+            for i in optParams:
+                result.append("    :param %s: %s\n    :type %s: %s - optional\n" % (i["name"], i["info"], i["name"], i["type"]))
+            
+        for i in outParams:
+            result.append("    :return: %s - %s\n    :rtype: %s\n" % (i["name"], i["info"], i["type"]))
+        
+        
+    #print("\n".join(result))
+    print("".join(result))
 if success:
     parse_parameters(globals()[name])
