1c6b04362c9d834fb625164d70e20fdbec36fdfc
Marc Gronle <gronle@ito.uni-stuttgart.de>
2016-01-19 17:38:09 +0100

    dataObject.zeros, ones, eye, randN and rand returned int8 as default type, but the documentation said uint8 (like the default constructor). This was a bug. It is fixed now, all static constructors return uint8 per default.

--- python_unittests/run_suite.py
+++ python_unittests/run_suite.py
@@ -1,9 +1,11 @@
 import unittest
 import dataobject_squeeze_reshape
+import dataobject_static_constructors
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest (unittest.makeSuite(dataobject_squeeze_reshape.DataObjectResize))
+    suite.addTest (unittest.makeSuite(dataobject_static_constructors.DataObjectStaticConstructors))
     return suite
     
 if (__name__ == "__main__"):
