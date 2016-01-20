e7e34e1799bc457419a3858a8fc6d784f1e34246
Marc Gronle <gronle@ito.uni-stuttgart.de>
2016-01-20 17:30:39 +0100

    demos added for creating different types of GUIs with Python.

---
+++ demo/ui/guiTypes/dockWidgetExamples.py
@@ -0,0 +1,13 @@
+'''create a dock widget form the main window at the bottom dock widget area
+The dock widget can be destroyed by deleting the variable, that references to it.
+
+If the dock widget is hidden by clicking the close button, it is hidden and can 
+be re-shown e.g. by the context menu of the toolbar of itom'''
+bottom_dock_window = ui("mainWindow.ui", ui.TYPEDOCKWIDGET, dockWidgetArea = ui.BOTTOMDOCKWIDGETAREA)
+
+'''create a dock widget form the main window at the bottom dock widget area
+The dock widget can be destroyed by deleting the variable, that references to it.
+
+If the dock widget is hidden by clicking the close button, it is hidden and can 
+be re-shown e.g. by the context menu of the toolbar of itom'''
+right_dock_widget = ui("widget.ui", ui.TYPEDOCKWIDGET, dockWidgetArea = ui.RIGHTDOCKWIDGETAREA)
