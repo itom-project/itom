eba5a0cf10ae55b3650ad8e7171b1cb02f6ca853
Marc Gronle <gronle@ito.uni-stuttgart.de>
2016-03-08 11:39:53 +0100

    unittest added (shallowCopyConvertCrash_Test) to proof a crash if a dataObject type conversion was forced whose destination object is a shallow copy of another dataObject. This bug is fixed in dataObject version > 1.5.0.0

---
+++ itom_unittests/unittest_dataobject/dataObjectFreeCreate_Test.cpp
@@ -0,0 +1,41 @@
+
+#include "../../Common/sharedStructures.h"
+
+//opencv
+#pragma warning( disable : 4996 ) //C:\OpenCV2.3\build\include\opencv2/flann/logger.h(70): warning C4996: 'fopen': This function or variable may be unsafe. Consider using fopen_s instead.
+#pragma once
+#include "opencv/cv.h"
+#include "../../DataObject/dataobj.h"
+#include "gtest/gtest.h"
+#include "commonChannel.h"
+
+template <typename _Tp> class DataObjectFreeCreate_Test : public ::testing::Test 
+    { 
+public:
+
+    virtual void SetUp(void)
+    {
+        
+    }
+    
+    virtual void TearDown(void){}
+};
+    
+
+TYPED_TEST_CASE(DataObjectFreeCreate_Test, ItomDataAllTypes);
+
+//shallowCopyConvertCrash_Test
+/*!
+    This test causes a crash for dataObject version <= 1.5.0.0 since the
+    reference counter has not been set to zero if the data object was freed
+    but not the last one (therefore the ref counter was decremented but not
+    deleted, nevertheless it has to be set to zero within this instance of
+    dataObject)
+*/
+TYPED_TEST(DataObjectFreeCreate_Test, shallowCopyConvertCrash_Test)
+{
+    ito::DataObject a(600, 600, ito::tComplex128);
+    ito::DataObject b = ito::DataObject(a);
+    ito::DataObject c(600, 600, ito::tFloat64);
+    c.convertTo(b, ito::tComplex128);
+};
