3a99f34cdec5ddca137111d4da8e685495f8fd6e
Marc Gronle <gronle@ito.uni-stuttgart.de>
2016-03-29 15:18:50 +0200

    demo about pickers in 1d plot added

---
+++ demo/plots/drawPickersPlot1D.py
@@ -0,0 +1,44 @@
+'''This demo shows how to programatically set pickers
+to the line of a 1D plot
+
+The most important slots for these features are implemented
+in the designer widget 'itom1dqwtplot':
+
+myPlotHandle.call("setPicker", axis-values [, curveIndex = 0, physicalCoordinates=True])
+myPlotHandle.call("appendPicker", axis-values [, curveIndex = 0, physicalCoordinates=True])
+myPlotHandle.call("deletePicker", [index])
+
+'setPicker' and 'appendPicker' are used to set a new set of pickers or append further
+pickers to the existing set of pickers. 'deletePicker' is used to clear existing pickers.
+
+For settings pickers, a list or tuple of axis-values has to be given. The corresponding value
+is calculated based on the current curve, that is displayed. In order to stick the new picker
+to another than the first curve, use another curveIndex parameter. Usually, the axis-values
+are considered to be given in physical coordinates (which are displayed in the coordinate system).
+
+If you want to give the coordinate of a picker in real pixel-coordinates of the dataObject,
+set 'physicalCoordinates' to False.
+'''
+
+import numpy as np
+
+#create demo data
+#1d sine
+sine = np.sin(np.arange(0, 10 * np.pi, (1/20) * np.pi))
+sine2 = np.sin(np.arange(0, 5 * np.pi, (1/40) * np.pi))
+twosines = dataObject([2,len(sine)],'float64')
+twosines[0,:] = sine
+twosines[1,:] = sine2
+
+twosines.axisScales=(1, np.pi/20)
+
+[i,h] = plot(twosines, 'itom1dqwtplot')
+
+#increase the maximum number of pickers to 7
+h["pickerLimit"] = 7
+
+#set two pickers to the first curve
+h.call("setPicker", (1.5 * np.pi, 2.5*np.pi), 0)
+
+#set two pickers to the second curve
+h.call("appendPicker", (40, 80, 120, 160, 200), 1, False)
