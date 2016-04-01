a37b28a2913c2d45c4e842395d08ae011e39ef26
Marc Gronle <gronle@ito.uni-stuttgart.de>
2016-04-01 13:41:46 +0200

    demo cloudAndMeshVisualization made a little bit smaller to avoid too much load for the graphics card

--- demo/ui/cloudAndMeshVisualization.py
+++ demo/ui/cloudAndMeshVisualization.py
@@ -8,7 +8,7 @@ except Exception as ex:
 
 #create a data objects with X, Y and Z values of a topography
 # as well as a 2.5D topography in terms of a data object
-[X,Y] = np.meshgrid(np.arange(0,500,0.1),np.arange(0,500,0.1))
+[X,Y] = np.meshgrid(np.arange(0,100,0.25),np.arange(0,100,0.25))
 Z = np.sin(X*2)+np.cos(Y*0.5)
 I = np.random.rand(*X.shape) #further intensity
 C = dataObject.randN([X.shape[0],X.shape[1]], 'rgba32') #further color information
