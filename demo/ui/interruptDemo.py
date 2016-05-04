d6b07672b5a47cfd0762293c884ba8ce2d704a60
Marc Gronle <gronle@ito.uni-stuttgart.de>
2016-05-04 15:52:16 +0200

    interrupt demo added

---
+++ demo/ui/interruptDemo.py
@@ -0,0 +1,20 @@
+import time
+
+gui = ui("interruptDemo.ui", ui.TYPEWINDOW)
+
+def do():
+    gui.btnDo["enabled"] = False
+    try:
+        #it is not possible to interrupt a simple sleep command
+        #therefore, it is put into a loop over shorter sleeps...
+        for i in range(0,200):
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("the sleep loop has been interrupted.")
+        gui.btnDo["enabled"] = True
+        raise
+    gui.btnDo["enabled"] = True
+
+gui.btnDo.connect("clicked()", do)
+gui.btnInterrupt.invokeKeyboardInterrupt("clicked()")
+gui.show()
