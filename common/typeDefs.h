3f088fd58ef513ffe065ddb96455ef20bac4f9cd
Marc Gronle <marc.gronle@gmx.de>
2016-05-23 23:22:49 +0200

    deprecation macro moved to typeDefs.h. Deprecated 'isZeroValue'... methods (that moved to numerics.h few month ago) removed from dataObjectFuncs.h

--- common/typeDefs.h
+++ common/typeDefs.h
@@ -149,6 +149,16 @@ namespace ito
     };
 
 
+#ifdef __GNUC__
+    #define DEPRECATED __attribute__((deprecated))
+#elif defined(_MSC_VER)
+    #define DEPRECATED __declspec(deprecated)
+#else
+    #pragma message("WARNING: You need to implement DEPRECATED for this compiler")
+    #define DEPRECATED
+#endif
+
+
 #ifndef WIN32
     #define _strdup strdup
     #define _itoa itoa
