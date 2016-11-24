fb4c522585087cf8064d02346eb5fd387961544b
Marc Gronle <gronle@ito.uni-stuttgart.de>
2016-11-24 18:41:27 +0100

    plugin widgets can now be opened by plugin dock widget even if they have mandatory or optional initialization parameters

---
+++ Qitom/ui/dialogOpenNewGui.cpp
@@ -0,0 +1,101 @@
+/* ********************************************************************
+    itom software
+    URL: http://www.uni-stuttgart.de/ito
+    Copyright (C) 2016, Institut fuer Technische Optik (ITO),
+    Universitaet Stuttgart, Germany
+
+    This file is part of itom.
+  
+    itom is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public Licence as published by
+    the Free Software Foundation; either version 2 of the Licence, or (at
+    your option) any later version.
+
+    itom is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library
+    General Public Licence for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with itom. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************** */
+
+#include "dialogOpenNewGui.h"
+
+#include <qgridlayout.h>
+#include <qsignalmapper.h>
+
+#include "../../common/helperCommon.h"
+#include "../helper/guiHelper.h"
+
+namespace ito {
+
+//----------------------------------------------------------------------------------------------------------------------------------
+DialogOpenNewGui::DialogOpenNewGui(const QString &widgetName, const QVector<ito::Param> &initParamsMand, const QVector<ito::Param> &initParamsOpt) :
+    QDialog(),
+    m_pMandParser(NULL),
+    m_pOptParser(NULL)
+{
+    ui.setupUi(this);
+
+    //get name
+    ui.lblWidgetName->setText(widgetName);
+
+    QWidget *canvas = new QWidget();
+    ui.scrollParamsMand->setWidget(canvas);
+    m_pMandParser = new ParamInputParser(canvas);
+
+    canvas = new QWidget();
+    ui.scrollParamsOpt->setWidget(canvas);
+    m_pOptParser = new ParamInputParser(canvas);
+
+    m_pMandParser->createInputMask(initParamsMand);
+    if (initParamsMand.size() == 0)
+    {
+        ui.tabWidget->setTabEnabled(0, false);
+    }
+
+    m_pOptParser->createInputMask(initParamsOpt);
+    if (initParamsOpt.size() == 0)
+    {
+        ui.tabWidget->setTabEnabled(1, false);
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------
+DialogOpenNewGui::~DialogOpenNewGui()
+{
+    DELETE_AND_SET_NULL(m_pMandParser);
+    DELETE_AND_SET_NULL(m_pOptParser);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------
+ito::RetVal DialogOpenNewGui::getFilledMandParams(QVector<ito::ParamBase> &params)
+{
+    return m_pMandParser->getParameters(params);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------
+ito::RetVal DialogOpenNewGui::getFilledOptParams(QVector<ito::ParamBase> &params)
+{
+    return m_pOptParser->getParameters(params);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------
+void DialogOpenNewGui::on_buttonBox_accepted()
+{
+    ito::RetVal retValue;
+    if (!m_pMandParser->validateInput(true, retValue, true))
+    {
+        return;
+    }
+
+    if (!m_pOptParser->validateInput(false, retValue, true))
+    {
+        return;
+    }
+    
+    emit accept();
+}
+
+} //end namespace ito
---
+++ Qitom/ui/dialogOpenNewGui.h
@@ -0,0 +1,66 @@
+/* ********************************************************************
+    itom software
+    URL: http://www.uni-stuttgart.de/ito
+    Copyright (C) 2016, Institut fuer Technische Optik (ITO),
+    Universitaet Stuttgart, Germany
+
+    This file is part of itom.
+  
+    itom is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public Licence as published by
+    the Free Software Foundation; either version 2 of the Licence, or (at
+    your option) any later version.
+
+    itom is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library
+    General Public Licence for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with itom. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************** */
+
+#ifndef DIALOGOPENNEWGUI_H
+#define DIALOGOPENNEWGUI_H
+
+#include "../../common/sharedStructures.h"
+
+#include "../global.h"
+
+#include <QtGui>
+#include <qdialog.h>
+
+#include "paramInputParser.h"
+
+
+#include "ui_dialogOpenNewGui.h"
+
+namespace ito {
+
+class DialogOpenNewGui : public QDialog 
+{
+    Q_OBJECT
+
+public:
+    DialogOpenNewGui(const QString &widgetName, const QVector<ito::Param> &initParamsMand, const QVector<ito::Param> &initParamsOpt);
+    ~DialogOpenNewGui();
+
+    ito::RetVal getFilledMandParams(QVector<ito::ParamBase> &params);
+    ito::RetVal getFilledOptParams(QVector<ito::ParamBase> &params);
+
+protected:
+    ParamInputParser *m_pMandParser;
+    ParamInputParser *m_pOptParser;
+
+private:
+
+    Ui::DialogOpenNewGui ui;
+
+private slots:
+    void on_buttonBox_accepted();
+
+};
+
+} //end namespace ito
+
+#endif
