256241760b1467cf8c36a3947a29960d0c669817
Marc Gronle <gronle@ito.uni-stuttgart.de>
2017-02-09 17:19:00 +0100

    :lipstick:

--- itom_unittests/unittest_addinmanager/AddInManagerTest.cpp
+++ itom_unittests/unittest_addinmanager/AddInManagerTest.cpp
@@ -9,10 +9,6 @@ TEST(AddInManagerTest, General)
     int argc = 0;
     QCoreApplication a(argc, NULL);
 
-    //ito::AddInManager* aim = ito::AddInManager::createInstance("", NULL, NULL, NULL); // &a);
-    //EXPECT_NE(aim, NULL);
-    //AddInManager::closeInstance();
-
     ito::AddInManager* aim = new ito::AddInManager("", NULL, NULL, NULL); // &a);
     EXPECT_FALSE(aim == NULL);
     aim->closeInstance();
