00435c140b83836313454f3674666bdd83fc4997
robin92 <hahn@ito.uni-stuttgart.de>
2017-02-22 13:12:02 +0100

    removed implementation of the stack function. In future the function is called dstack and is a static dataObject function. Functions are already declared put not implemented...

--- DataObject/dataobjVersion.h
+++ DataObject/dataobjVersion.h
@@ -1,7 +1,7 @@
 /* ********************************************************************
     itom software
     URL: http://www.uni-stuttgart.de/ito
-    Copyright (C) 2016, Institut fuer Technische Optik (ITO),
+    Copyright (C) 2017, Institut fuer Technische Optik (ITO),
     Universitaet Stuttgart, Germany
 
     This file is part of itom and its software development toolkit (SDK).
@@ -25,6 +25,6 @@
     along with itom. If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************** */
 
-#define DATAOBJ_VER_STRING  "1.7.5.0"
+#define DATAOBJ_VER_STRING  "1.7.6.0"
 
 
