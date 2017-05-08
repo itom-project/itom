da75ba4470bb6bf5ee7c968bdb76089dc4fd1826
Johann Krauter <krauter@ito.uni-stuttgart.de>
2017-05-08 15:28:06 +0200

    added files for MSVC2015 compilation

---
+++ compile_release32_v14.bat
@@ -0,0 +1,8 @@
+TITLE compile release x86 - %~dp0
+CALL "%VS140COMNTOOLS%\vsvars32.bat" x86
+
+msbuild.exe "%~dp0\ALL_BUILD.vcxproj" /p:configuration=release /p:platform=win32
+
+pause
+
+REM for rebuild add "/t:rebuild" after /p:platform=win32
