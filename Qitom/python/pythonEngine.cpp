7bddba7e971a8e2b598656ae00be17abb6bd152e
itobiege <itobiege@ito.uni-stuttgart.de>
2017-05-11 12:58:31 +0200

    added debug functions

--- Qitom/python/pythonEngine.cpp
+++ Qitom/python/pythonEngine.cpp
@@ -315,6 +315,7 @@ void PythonEngine::pythonSetup(ito::RetVal *retValue)
         {
             QString pythonSubDir = QCoreApplication::applicationDirPath() + QString("/python%1").arg(PY_MAJOR_VERSION);
             QString pythonAllInOneDir = QCoreApplication::applicationDirPath() + QString("../../3rdParty/Python");
+            qDebug() << "pythonAllInOneDir:" << pythonAllInOneDir;
             //check if an alternative home directory of Python should be set:
             QSettings settings(AppManagement::getSettingsFile(), QSettings::IniFormat);
             settings.beginGroup("Python");
@@ -342,6 +343,8 @@ void PythonEngine::pythonSetup(ito::RetVal *retValue)
 #else
                 pythonDirState = 1;
 #endif
+                qDebug() << "pythonDirState:" << pythonDirState;
+                qDebug() << "pythonHomeFromSettings:" << pythonHomeFromSettings;
                 settings.setValue("pyDirState", pythonDirState);
             }
 
