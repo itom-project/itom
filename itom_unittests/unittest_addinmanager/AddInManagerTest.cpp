74cf268449926a636952f10bbf3fc9b5df294fc7
Chris <chr.kohler@gmail.com>
2017-09-13 19:51:21 -0300

    bugfix in unicode version of setdllpath (in main.cpp)

--- itom_unittests/unittest_addinmanager/AddInManagerTest.cpp
+++ itom_unittests/unittest_addinmanager/AddInManagerTest.cpp
@@ -84,18 +84,7 @@ TEST(AddInManagerTest, General)
 #if WINVER > 0x0502 
     if (QSysInfo::windowsVersion() > QSysInfo::WV_XP)
     {
-#if UNICODE
-        //sometimes LoadLibrary commands in plugins with files that are located in the lib folder cannot be loaded
-        //even if the lib folder is add to the path variable in this funtion, too. The SetDllDirectory
-        //is another approach to reach this (only available since Win XP).
-        wchar_t *lib_path = new wchar_t[libDir.size() + 5];
-        memset(lib_path, 0, (libDir.size() + 5) * sizeof(wchar_t));
-        libDir.toWCharArray(lib_path);
-        SetDllDirectory(lib_path);
-        delete lib_path;
-#else
-        SetDllDirectory(libDir.toLatin1().data());
-#endif
+        SetDllDirectoryA(libDir.toLatin1().data());
     }
 #endif
     strcat(newpath, libDir.toLatin1().data()); //set libDir at the beginning of the path-variable
