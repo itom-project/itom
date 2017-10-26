7c242de4e9cb19dbcfac19a5838d5b2112d4815b
M. Gronle <marc.gronle@de.trumpf.com>
2017-10-26 13:22:31 +0200

    matplotlib example updated to be compatible with matplotlib 2.1.0

--- demo/matplotlib/plot_ggplot.py
+++ demo/matplotlib/plot_ggplot.py
@@ -15,6 +15,9 @@ import matplotlib.pyplot as plt
 
 plt.style.use('ggplot')
 
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
 fig, axes = plt.subplots(ncols=2, nrows=2)
 ax1, ax2, ax3, ax4 = axes.ravel()
 
@@ -25,7 +28,7 @@ ax1.plot(x, y, 'o')
 # sinusoidal lines with colors from default color cycle
 L = 2*np.pi
 x = np.linspace(0, L)
-ncolors = len(plt.rcParams['axes.color_cycle'])
+ncolors = len(plt.rcParams['axes.prop_cycle'])
 shift = np.linspace(0, L, ncolors, endpoint=False)
 for s in shift:
     ax2.plot(x, np.sin(x + s), '-')
@@ -36,14 +39,15 @@ x = np.arange(5)
 y1, y2 = np.random.randint(1, 25, size=(2, 5))
 width = 0.25
 ax3.bar(x, y1, width)
-ax3.bar(x + width, y2, width, color=plt.rcParams['axes.color_cycle'][2])
+ax3.bar(x + width, y2, width,
+        color=list(plt.rcParams['axes.prop_cycle'])[2]['color'])
 ax3.set_xticks(x + width)
 ax3.set_xticklabels(['a', 'b', 'c', 'd', 'e'])
 
 # circles with colors from default color cycle
-for i, color in enumerate(plt.rcParams['axes.color_cycle']):
+for i, color in enumerate(plt.rcParams['axes.prop_cycle']):
     xy = np.random.normal(size=2)
-    ax4.add_patch(plt.Circle(xy, radius=0.3, color=color))
+    ax4.add_patch(plt.Circle(xy, radius=0.3, color=color['color']))
 ax4.axis('equal')
 ax4.margins(0)
 
