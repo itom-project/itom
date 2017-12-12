30a3c95e7fcc244eb4b4ca8759e1866c1188d229
robin92 <hahn@ito.uni-stuttgart.de>
2017-12-12 18:12:44 +0100

    plotxy: demo modified

--- demo/plots/plotXY.py
+++ demo/plots/plotXY.py
@@ -1,16 +1,22 @@
 # coding=iso-8859-15
+''' This demo shows how to set an optional x-vector to an 1D-plot.
+
+The optional x-vector can be set by passing the optional x-vector as second argument to the plot1 function
+or by setting the property xVector of an existing plot.
+If you want to add an x-vector to a plot of an nxm dataObject your x-vector has to be an dataObject of shape 1xm.
+Once you add an x-vector with a last dimension greater m the last points will be ignored.'''
 import numpy as np
+angle=np.linspace(0,10*np.pi)
+x = dataObject([1,angle.shape[0]],dtype='float32')
+y = dataObject([1,angle.shape[0]],dtype='float32')
+cnt=0
+for theta in angle:
+    r = ((theta)**2)
+    x[0,cnt]=(r*np.cos(theta))
+    y[0,cnt]=(r*np.sin(theta))
+    cnt+=1
 
-xVec = np.arange(0, 6*np.pi, np.pi/100)
-yVec = np.sin(xVec)
+plot1(y,x)
 
-xVec = dataObject(xVec)
-yVec = dataObject(yVec)
-
-yVec.valueDescription = "amplitude"
-yVec.valueUnit = "a. u."
-yVec.setAxisDescription(1, "time")
-yVec.setAxisUnit(1, "s")
-yVec.setTag("title", "sin function")
-
-plot1(yVec, xVec)
+fig=plot(y)
+fig[-1]['xVector']=x
