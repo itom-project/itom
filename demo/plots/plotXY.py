0103da968b411d33809333dbb3fbe83ae087f03a
Marc Gronle <marc.gronle@gmx.de>
2018-01-07 16:56:22 +0100

    improved demo plotXY.py

--- demo/plots/plotXY.py
+++ demo/plots/plotXY.py
@@ -6,15 +6,13 @@ or by setting the property xData of an existing plot.
 If you want to add an x-vector to a plot of an nxm dataObject your x-vector has to be an dataObject of shape 1xm.
 Once you add an x-vector with a last dimension greater m the last points will be ignored.'''
 import numpy as np
-angle=np.linspace(0,10*np.pi)
+angle=np.linspace(0,10*np.pi, num=500)
 x = dataObject([1,angle.shape[0]],dtype='float32')
 y = dataObject([1,angle.shape[0]],dtype='float32')
-cnt=0
-for theta in angle:
-    r = ((theta)**2)
-    x[0,cnt]=(r*np.cos(theta))
-    y[0,cnt]=(r*np.sin(theta))
-    cnt+=1
+
+radius = angle**2
+x[:,:] = (radius * np.cos(angle)).astype('float32')
+y[:,:] = (radius * np.sin(angle)).astype('float32')
 
 #alternative 1: use the itom.plot1 method
 plot1(y,x)
@@ -45,10 +43,8 @@ xPoints[0,2]=1
 yPoints[0,3]=1
 xPoints[0,3]=0
 #second line
-yPoints[1,0]=0
-xPoints[1,0]=0
-yPoints[1,1]=1
-xPoints[1,1]=1
+yPoints[1,0:2] = (0,1)
+xPoints[1,0:2] = (0,1)
 #third line 
 yPoints[2,0]=1
 xPoints[2,0]=0
@@ -65,23 +61,13 @@ xPoints[3,2]=1
 plot1(yPoints, xPoints)
 
 ######################################################################################
-'''this section demonstrates how to draw multiple lines with a common xVector'''
+'''this section demonstrates how to draw multiple lines with a common array of x-coordinates'''
 ######################################################################################
 multipleY = dataObject([2,4],'float32')
 singleX = dataObject([1,9], 'float32') #it is also possible to use a too long xData set. The last values will be ignored
 
-multipleY[0,0]=0
-singleX[0,0]=0
-multipleY[0,1]=0
-singleX[0,1]=1
-multipleY[0,2]=1
-singleX[0,2]=0.5
-multipleY[0,3]=0
-singleX[0,3]=0
-
-multipleY[1,0]=1
-multipleY[1,1]=1
-multipleY[1,2]=1.5
-multipleY[1,3]=1
+singleX[0,0:4] = (0,1,0.5, 0)
+multipleY[0,:] = (0,0,1,0)
+multipleY[1,:] = (1,1,1.5,1)
 
 plot1(multipleY,singleX)
