a37c43f5b7631985e26c334dc56e92cae02b705f
robin92 <hahn@ito.uni-stuttgart.de>
2018-02-14 08:35:26 +0100

    update year :lipstick:

--- itomWidgets/menuComboBox.h
+++ itomWidgets/menuComboBox.h
@@ -1,7 +1,7 @@
 /* ********************************************************************
     itom software
     URL: http://www.uni-stuttgart.de/ito
-    Copyright (C) 2016, Institut fuer Technische Optik (ITO),
+    Copyright (C) 2018, Institut fuer Technische Optik (ITO),
     Universitaet Stuttgart, Germany
 
     This file is part of itom and its software development toolkit (SDK).
