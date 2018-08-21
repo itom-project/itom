50cb1344aa48bf5146141521afe20e12ad6e9ef2
robin92 <hahn@ito.uni-stuttgart.de>
2018-08-21 08:50:19 +0200

    renamed contourLines demo

--- demo/plots/demoContourLines2dPlot.py
+++ demo/plots/demoContourLines2dPlot.py
@@ -1,7 +1,7 @@
 # coding=iso-8859-15
 ''' This demo shows how to display contour lines in an itom2dQwtPlot.
 
-The contour levels can be set via the contourLevels property. The expected type is a dataObject of any shape of type uint8, int8, uint16, int16, int32,
+The contour levels can be set via the contourLevels property. The expected type is a dataObject of shape (1xn) of type uint8, int8, uint16, int16, int32,
 float32 or float64. The line width and color map can be adjusted setting the property contourLineWidth and contourColorMap respectively.'''
 
 import numpy as np
@@ -10,7 +10,10 @@ x,y = np.meshgrid(vec,vec)
 r= np.sqrt(x**2+y**2)
 
 pl = plot(r)
-levels=dataObject.randN([2,1,2,2],'uint8')
+levels=dataObject.randN([1,3],'uint8')
+levels[0,0]=50
+levels[0,1]=75
+levels[0,2]=250
 
 pl[1]['contourLevels']=levels
 pl[1]['contourColorMap']='hotIron'
