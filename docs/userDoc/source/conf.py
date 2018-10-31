3e48a485da2c269950c91f85e5d4fd5d06ef42f8
Johann Krauter <krauter@ito.uni-stuttgart.de>
2018-10-31 13:28:16 +0100

    :lipsick: sorry wrong branch before

--- docs/userDoc/source/conf.py
+++ docs/userDoc/source/conf.py
@@ -35,7 +35,7 @@ needs_sphinx = '1.4.3'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'breathe', 'sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'numpydoc', 'breathe', 'sphinx.ext.intersphinx', 'itomext.designerplugindoc']
 
 if sphinx.__version__ >= "0.7":
     extensions.append('sphinx.ext.autosummary')
