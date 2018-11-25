5fb841ae04c69cf1385651f0278825e02c4dd967
M. Gronle <marc.gronle@gmx.de>
2018-11-25 17:13:25 +0100

    :lipstick:

--- Qitom/widgets/scriptDockWidget.cpp
+++ Qitom/widgets/scriptDockWidget.cpp
@@ -1094,8 +1094,10 @@ void ScriptDockWidget::tabContextMenuEvent(QContextMenuEvent * event)
 //! updates actions which deal with editor commands
 void ScriptDockWidget::updateEditorActions()
 {
+	int tabCount = m_tab->count();
+
     m_saveAllScriptsAction->setEnabled(false);
-    for (int i = 0; i < m_tab->count(); i++)
+    for (int i = 0; i < tabCount; i++)
     {
         if (static_cast<ScriptEditorWidget *>(m_tab->widget(i))->isModified())
         {
@@ -1110,18 +1112,18 @@ void ScriptDockWidget::updateEditorActions()
         sew = static_cast<ScriptEditorWidget *>(m_tab->widget(m_actTabIndex));
     }
 
-    m_saveScriptAction->setEnabled(m_tab->count()>0 && sew != NULL && sew->isModified());
-    m_saveScriptAsAction->setEnabled(m_tab->count()>0 && sew != NULL);
+    m_saveScriptAction->setEnabled(tabCount>0 && sew != NULL && sew->isModified());
+    m_saveScriptAsAction->setEnabled(tabCount>0 && sew != NULL);
 
     m_cutAction->setEnabled(sew != NULL && sew->getCanCopy());
     m_copyAction->setEnabled(sew != NULL && sew->getCanCopy());
-    m_pasteAction->setEnabled(true, true); //!< todo
+    m_pasteAction->setEnabled(tabCount > 0 && sew != NULL);
     m_undoAction->setEnabled(sew != NULL && sew->isUndoAvailable());
     m_redoAction->setEnabled(sew != NULL && sew->isRedoAvailable());
-    m_commentAction->setEnabled(m_tab->count()>0 && sew != NULL);
-    m_uncommentAction->setEnabled(m_tab->count()>0 && sew != NULL);
-    m_indentAction->setEnabled(m_tab->count()>0 && sew != NULL);
-    m_unindentAction->setEnabled(m_tab->count()>0 && sew != NULL);
+    m_commentAction->setEnabled(tabCount > 0 && sew != NULL);
+    m_uncommentAction->setEnabled(tabCount > 0 && sew != NULL);
+    m_indentAction->setEnabled(tabCount > 0 && sew != NULL);
+    m_unindentAction->setEnabled(tabCount > 0 && sew != NULL);
 
     m_tabCloseAction->setEnabled(m_actTabIndex > -1);
     m_tabCloseAllAction->setEnabled(m_actTabIndex > -1);
@@ -1738,7 +1740,7 @@ void ScriptDockWidget::mnuTabDock()
 void ScriptDockWidget::mnuTabUndock()
 {
     bool undockToNewScriptWindow = !docked();
-    emit (undockScriptTab(this, m_actTabIndex, undockToNewScriptWindow, !docked()));
+	emit(undockScriptTab(this, m_actTabIndex, undockToNewScriptWindow, true)); // !docked()));
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------
