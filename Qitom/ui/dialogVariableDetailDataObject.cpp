33d700491295d7bd4dbb7985a8b5243aba1001f9
Johann <johannkrauter@googlemail.com>
2021-04-26 22:31:19 +0200

    first working version of displaying a dataObject in the new DialogVariableDetailDataObject

---
+++ Qitom/ui/dialogVariableDetailDataObject.cpp
@@ -0,0 +1,56 @@
+/* ********************************************************************
+    itom software
+    URL: http://www.uni-stuttgart.de/ito
+    Copyright (C) 2020, Institut fuer Technische Optik (ITO),
+    Universitaet Stuttgart, Germany
+
+    This file is part of itom.
+  
+    itom is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public Licence as published by
+    the Free Software Foundation; either version 2 of the Licence, or (at
+    your option) any later version.
+
+    itom is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library
+    General Public Licence for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with itom. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************** */
+
+#include "dialogVariableDetailDataObject.h"
+#include <qsharedpointer.h>
+#include "dataobj.h"
+
+#include <qclipboard.h>
+
+namespace ito
+{
+
+//----------------------------------------------------------------------------------------------
+DialogVariableDetailDataObject::DialogVariableDetailDataObject(
+    const QString& name,
+    const QString& type,
+    QSharedPointer<ito::DataObject> data,
+    QWidget* parent):
+    QDialog(parent)
+{
+    ui.setupUi(this);
+    
+    ui.txtName->setText(name);
+    ui.txtType->setText(type);
+    ui.table->setData(data);
+    ui.table->setReadOnly(true);
+    
+}
+
+//----------------------------------------------------------------------------------------------
+void DialogVariableDetailDataObject::on_btnCopyClipboard_clicked()
+{
+    QClipboard *clipboard = QApplication::clipboard();
+    clipboard->setText(ui.txtName->text(), QClipboard::Clipboard);
+}
+
+} //end namespace ito
