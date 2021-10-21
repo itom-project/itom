c833bb730bffe99fdf5e90d45564a6bcb1e073cd
Johann Krauter <johannkrauter@googlemail.com>
2021-10-21 09:34:33 +0200

    Version switch for QOperatingSystemVersion

--- itom_unittests/unittest_addinmanager/AddInManagerTest.cpp
+++ itom_unittests/unittest_addinmanager/AddInManagerTest.cpp
@@ -10,7 +10,11 @@
 #include "gtest/gtest.h"
 #include <qcoreapplication.h>
 #include <qfileinfo.h>
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 9, 0))
 #include <qoperatingsystemversion.h>
+#else
+
+#endif
 
 //----------------------------------------------------------------------------------------------------------------------------------
 /*!
@@ -84,7 +88,11 @@ TEST(AddInManagerTest, General)
     strcat(newpath, "path=");
 
 #if WINVER > 0x0502
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 9, 0))
     if (QOperatingSystemVersion::current() >= QOperatingSystemVersion::Windows7)
+#else
+    if (QSysInfo::windowsVersion() > QSysInfo::WV_XP)
+#endif
     {
         SetDllDirectoryA(libDir.toLatin1().data());
     }
