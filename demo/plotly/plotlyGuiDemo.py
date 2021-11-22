35906201b9323e0a3303ea91069bcbec35083a92
Johann Krauter <johannkrauter@googlemail.com>
2021-11-22 07:04:02 +0100

    added parallel coordinates plot to plotly demo

--- demo/plotly/plotlyGuiDemo.py
+++ demo/plotly/plotlyGuiDemo.py
@@ -7,7 +7,6 @@ from itom import ui
 
 
 class PlotlyGuiDemo(ItomUi):
-    
     def __init__(self):
         """Constructor."""
         ItomUi.__init__(self, "plotlyGuiDemo.ui", ui.TYPEWINDOW, deleteOnClose=True)
@@ -24,9 +23,9 @@ class PlotlyGuiDemo(ItomUi):
         https://plotly.com/python/bar-charts/
         """
         long_df = px.data.medals_long()
-        
+
         fig = px.bar(long_df, x="nation", y="count", color="medal", title="Long-Form Input")
-        fig.show(plotHandle = self.plotlyPlot)
+        fig.show(plotHandle=self.plotlyPlot)
 
     @ItomUi.autoslot("")
     def on_btnPlot2_clicked(self):
@@ -35,23 +34,53 @@ class PlotlyGuiDemo(ItomUi):
         https://plotly.com/python/distplot/
         """
         df = px.data.tips()
-        fig = px.histogram(df, x="total_bill", y="tip", color="sex", marginal="rug",
-                           hover_data=df.columns)
-        fig.show(plotHandle = self.plotlyPlot)
+        fig = px.histogram(df, x="total_bill", y="tip", color="sex", marginal="rug", hover_data=df.columns)
+        fig.show(plotHandle=self.plotlyPlot)
 
     @ItomUi.autoslot("")
     def on_btnPlot3_clicked(self):
         """From the animations demo of plotly.
-    
+
         https://plotly.com/python/animations/
         """
         import plotly.express as px
-        
+
         df = px.data.gapminder()
-        
-        fig = px.bar(df, x="continent", y="pop", color="continent",
-          animation_frame="year", animation_group="country", range_y=[0,4000000000])
-        fig.show(plotHandle = self.plotlyPlot)
+
+        fig = px.bar(
+            df,
+            x="continent",
+            y="pop",
+            color="continent",
+            animation_frame="year",
+            animation_group="country",
+            range_y=[0, 4000000000],
+        )
+        fig.show(plotHandle=self.plotlyPlot)
+
+    @ItomUi.autoslot("")
+    def on_btnPlot4_clicked(self):
+        """From the animations demo of plotly.
+
+        https://plotly.com/python/parallel-coordinates-plot/
+        """
+        import plotly.express as px
+
+        df = px.data.iris()
+        fig = px.parallel_coordinates(
+            df,
+            color="species_id",
+            labels={
+                "species_id": "Species",
+                "sepal_width": "Sepal Width",
+                "sepal_length": "Sepal Length",
+                "petal_width": "Petal Width",
+                "petal_length": "Petal Length",
+            },
+            color_continuous_scale=px.colors.diverging.Tealrose,
+            color_continuous_midpoint=2,
+        )
+        fig.show(plotHandle=self.plotlyPlot)
 
 
 # create a first instance of AutoConnectExample and the gui
