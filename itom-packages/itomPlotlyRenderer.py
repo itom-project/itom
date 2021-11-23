bce767d5979a6fd7fa95a5ba3edda2973864444c
M. Gronle <marc.gronle@gmx.de>
2021-11-23 23:19:09 +0100

    updates of plotly gui demo including the corresponding renderer

--- itom-packages/itomPlotlyRenderer.py
+++ itom-packages/itomPlotlyRenderer.py
@@ -22,18 +22,12 @@ class ItomPlotlyRenderer(ExternalRenderer):
         self,
         config=None,
         auto_play=False,
-        using=None,
-        new=0,
-        autoraise=True,
         post_script=None,
         animation_opts=None,
     ):
 
         self.config = config
         self.auto_play = auto_play
-        self.using = using
-        self.new = new
-        self.autoraise = autoraise
         self.post_script = post_script
         self.animation_opts = animation_opts
         self._plotHandle = None
