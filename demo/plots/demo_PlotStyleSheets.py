00b2e297385b7a5535c94e416568b1103756a325
Johann Krauter <johannkrauter@googlemail.com>
2022-04-26 19:32:46 +0200

    added demo plot style sheets

--- demo/plots/demo_PlotStyleSheets.py
+++ demo/plots/demo_PlotStyleSheets.py
@@ -1,9 +1,21 @@
+"""Plot style sheets
+==================
+
+This demo shows how you can set and change the theme of your GUI.
+The style can not be entirely removed to the windows, mac or plastique style
+(usually done by ``setStyle(new QWindowsStyle())``). 
+It only resets the style sheet to an empty one, such that the native fallbacks to the
+os dependent style is applied. However, ``Qt::WA_StyledBackground`` is still active.
+"""
+
 from itom import dataObject
 from itom import ui
 from itomUi import ItomUi
 import inspect
 import os.path
 import numpy as np
+
+# sphinx_gallery_thumbnail_path = '11_demos/_static/_thumb/demoPlotStyleSheet.png'
 
 
 class PlotStyleSheets(ItomUi):
@@ -16,19 +28,13 @@ class PlotStyleSheets(ItomUi):
         obj = dataObject.randN([1024, 1024], "float32")
         obj[200:300, 200:300] = float("nan")
         self.gui.plot2d["source"] = obj
-        self.gui.plot1d["source"] = dataObject(
-            np.sin(np.arange(0, 10 * np.pi, (1 / 20) * np.pi))
-        )
+        self.gui.plot1d["source"] = dataObject(np.sin(np.arange(0, 10 * np.pi, (1 / 20) * np.pi)))
 
-    def show(self, modalLevel=0):
-        ret = self.gui.show(modalLevel)
+    def show(self, modalLevel: int = 0):
+        self.gui.show(modalLevel)
 
     @ItomUi.autoslot("")
     def on_radioNoStyle_clicked(self):
-        # up to now, the style can not be entirely removed to the windows, mac or plastique style
-        # (usually done by setStyle(new QWindowsStyle()))
-        # It only resets the style sheet to an empty one, such that the native fallbacks to the
-        # os dependent style is applied. However, Qt::WA_StyledBackground is still active.
         self.gui["styleSheet"] = ""
         self.gui.plot2d["backgroundColor"] = "#ffffff"
         self.gui.plot2d["axisColor"] = "#000000"
@@ -63,11 +69,14 @@ class PlotStyleSheets(ItomUi):
         self.gui.plot1d["buttonSet"] = "StyleDark"
 
 
-def userdemo_plotStyleSheets():
-    instance = PlotStyleSheets()
-    instance.show()
-
-
 if __name__ == "__main__":
     instance = PlotStyleSheets()
     instance.show()
+
+###############################################################################
+# .. image:: ../_static/demoPlotStyleSheets_1.png
+#    :width: 100%
+
+###############################################################################
+# .. image:: ../_static/demoPlotStyleSheets_2.png
+#    :width: 100%
