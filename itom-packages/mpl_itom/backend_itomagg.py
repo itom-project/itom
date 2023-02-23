6678d7337bbcdc71a8dbc41a1d353d922dd8e482
Johann Krauter <johannkrauter@googlemail.com>
2023-02-23 08:01:18 +0100

    Matplotlib 3.7 adaptation works also for 3.6

--- itom-packages/mpl_itom/backend_itomagg.py
+++ itom-packages/mpl_itom/backend_itomagg.py
@@ -4,7 +4,7 @@ if matplotlib.__version__ < "2.0.0":
     from .backend_itomagg_v1 import *
 elif matplotlib.__version__ < "3.0.0":
     from .backend_itomagg_v2 import *
-elif matplotlib.__version__ < "3.6.0":
+elif matplotlib.__version__ < "3.7.0":
     from .backend_itomagg_v3 import *
 else:
-    from .backend_itomagg_v3_6 import *
+    from .backend_itomagg_v3_7 import *
