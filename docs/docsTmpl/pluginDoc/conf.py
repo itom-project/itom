7005c0b951f8b5e83c19b0f0d62dd7336ad4968d
photoniker <johannkrauter@googlemail.com>
2024-04-26 08:33:53 +0200

    upgrade of intersphinx mapping objects file

--- docs/docsTmpl/pluginDoc/conf.py
+++ docs/docsTmpl/pluginDoc/conf.py
@@ -314,35 +314,35 @@ intersphinx_mapping = {
         "https://docs.python.org/3",
         (
             None,
-            "..\\..\\..\\..\\sources\\itom\\docs\\userDoc\\source\\python3.8-inv.txt",
+            "..\\..\\..\\..\\sources\\itom\\docs\\userDoc\\source\\python3.12-inv.txt",
         ),
     ),
     "numpy": (
         "https://numpy.org/doc/stable",
         (
             None,
-            "..\\..\\..\\..\\sources\\itom\\docs\\userDoc\\source\\numpy1.22-inv.txt",
+            "..\\..\\..\\..\\sources\\itom\\docs\\userDoc\\source\\numpy1.26-inv.txt",
         ),
     ),
     "scipy": (
         "https://docs.scipy.org/doc/scipy",
         (
             None,
-            "..\\..\\..\\..\\sources\\itom\\docs\\userDoc\\source\\scipy1.6.1-inv.txt",
+            "..\\..\\..\\..\\sources\\itom\\docs\\userDoc\\source\\scipy1.13.0-inv.txt",
         ),
     ),
     "matplotlib": (
         "https://matplotlib.org",
         (
             None,
-            "..\\..\\..\\..\\sources\\itom\\docs\\userDoc\\source\\matplotlib3.5-inv.txt",
+            "..\\..\\..\\..\\sources\\itom\\docs\\userDoc\\source\\matplotlib3.8.4-inv.txt",
         ),
     ),
     "pandas": (
         "https://pandas.pydata.org/docs/",
         (
             None,
-            "..\\..\\..\\..\\sources\\itom\\docs\\userDoc\\source\\pandas1.3.5-inv.txt",
+            "..\\..\\..\\..\\sources\\itom\\docs\\userDoc\\source\\pandas2.2.2-inv.txt",
         ),
     ),
 }
