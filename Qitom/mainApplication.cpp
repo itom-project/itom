b8e77e7affa6766d5506353722747c823427570c
M. Gronle <2607303+magro11@users.noreply.github.com>
2024-06-19 00:08:40 +0200

    bugfix, ==operator for simple C-char-arrays is a start pointer comparison, no string comparison. The previous version did not work for the newest VS2022 version.

--- Qitom/mainApplication.cpp
+++ Qitom/mainApplication.cpp
@@ -138,9 +138,9 @@ MainApplication::MainApplication(tGuiType guiType) :
     QCoreApplication::setApplicationName("itom");
     QCoreApplication::setApplicationVersion(ITOM_VERSION_STR);
 
-    if(ITOM_ADDITIONAL_EDITION_NAME == "")
+    if(QByteArray(ITOM_ADDITIONAL_EDITION_NAME) == "")
     {
-        if(ITOM_VERSION_STR == "0.0.0" || ITOM_VERSION_IDENTIFIERS == "dev" )
+        if(QByteArray(ITOM_VERSION_STR) == "0.0.0" || QByteArray(ITOM_VERSION_IDENTIFIERS) == "dev" )
         {
             m_devFlag = true;
         }
