ea8e76e8a8130b996325ffe57657ef4f261f9335
itobiege <itobiege@ito.uni-stuttgart.de>
2024-06-19 08:19:16 +0200

    updated german translation

--- AddInManager/translation/addinmanager_de.ts
+++ AddInManager/translation/addinmanager_de.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de">
+<TS version="2.1" language="de_DE">
 <context>
     <name>QObject</name>
     <message>
@@ -528,6 +528,10 @@
         <location filename="../paramHelper.cpp" line="2629"/>
         <source>Parameter is no array type. Indexing not possible.</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Paramater is no array type. Indexing not possible.</source>
+        <translation type="obsolete">Der Parameter ist nicht vom Typ Array. Ein Indizierung ist nicht möglich.</translation>
     </message>
     <message>
         <source>Paramater is no array type. Indexing not possible.</source>
