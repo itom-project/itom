da5cb55455fb95fd63864bce5b763ee8b2602131
Bernd Bertschinger <bernd.bertschinger@ito.uni-stuttgart.de>
2026-04-15 19:08:39 +0200

    - Set QT default Build Mode to 6.0 - Changed CMake minimum Version to 3.29 - Updated FindGenICam.cmake to GeniCam Version 3.5

--- cmake/FindITOM_SDK.cmake
+++ cmake/FindITOM_SDK.cmake
@@ -20,7 +20,7 @@
 #
 #----------------------------------------------------------
 
-cmake_minimum_required(VERSION 3.1...3.15)
+cmake_minimum_required(VERSION 3.12...3.29)
 
 if(${CMAKE_VERSION} VERSION_LESS 3.12)
     cmake_policy(VERSION ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION})
